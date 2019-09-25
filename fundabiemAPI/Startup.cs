9fda6818615707bbe0058a9d9b4fb5a443c29e48
Guillermo Pisqui <guillermo.pisqui@hotmail.com>
2019-09-24 22:14:14 -0600

    agregue swager a API

--- fundabiemAPI/Startup.cs
+++ fundabiemAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace fundabiemAPI
 {
@@ -26,11 +27,35 @@ namespace fundabiemAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(swagger =>
+            {
+                var contact = new Contact() { Name = SwaggerConfiguration.ContactName, Url = SwaggerConfiguration.ContactUrl };
+                swagger.SwaggerDoc(SwaggerConfiguration.DocNameV1,
+                                   new Info
+                                   {
+                                       Title = SwaggerConfiguration.DocInfoTitle,
+                                       Version = SwaggerConfiguration.DocInfoVersion,
+                                       Description = SwaggerConfiguration.DocInfoDescription,
+                                       Contact = contact
+                                   }
+                                    );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(SwaggerConfiguration.EndpointUrl, SwaggerConfiguration.EndpointDescription);
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -42,6 +67,7 @@ namespace fundabiemAPI
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
---
+++ fundabiemAPI/SwaggerConfiguration.cs
@@ -0,0 +1,44 @@
+﻿public class SwaggerConfiguration
+{
+
+    /// <summary>
+    /// <para>Foo API v1</para>
+    /// </summary>
+    public const string EndpointDescription = "FUNDABIEN API v1";
+
+    /// <summary>
+    /// <para>/swagger/v1/swagger.json</para>
+    /// </summary>
+    public const string EndpointUrl = "/swagger/v1/swagger.json";
+
+    /// <summary>
+    /// <para>Jorge Serrano</para>
+    /// </summary>
+    public const string ContactName = "SEMINARIO";
+
+    /// <summary>
+    /// <para>https://github.com/guillermopis</para>
+    /// </summary>
+    public const string ContactUrl = "https://github.com/guillermopis";
+
+    /// <summary>
+    /// <para>v1</para>
+    /// </summary>
+    public const string DocNameV1 = "v1";
+
+    /// <summary>
+    /// <para>Foo API</para>
+    /// </summary>
+    public const string DocInfoTitle = "FUNDABIEM API";
+
+    /// <summary>
+    /// <para>v1</para>
+    /// </summary>
+    public const string DocInfoVersion = "v1";
+
+    /// <summary>
+    /// <para>Foo Api - Sample Web API in ASP.NET Core 2</para>
+    /// </summary>
+    public const string DocInfoDescription = "* FUNDABIEN API *";
+
+}
