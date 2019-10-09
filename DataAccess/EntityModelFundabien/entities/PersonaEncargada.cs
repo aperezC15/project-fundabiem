da0426c0fe579f51aea35745ff7878d41afac952
Daniel Estupe <upegonzalez897@gmail.com>
2019-10-08 20:36:30 -0600

    Se han agregado los modelos para completar el documento 'Registros Médicos'

--- DataAccess/EntityModelFundabien/entities/PersonaEncargada.cs
+++ DataAccess/EntityModelFundabien/entities/PersonaEncargada.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityModelFundabien.entities
 {
     public class PersonaEncargada
     {
+        [Key]
         public double idPersonaEncargada { get; set; }
         public double idPaciente { get; set; }
         public double idPersona { get; set; }
@@ -13,5 +15,6 @@ namespace EntityModelFundabien.entities
         public bool estaActivo { get; set; }
 
         public Persona persona { get; set; }
+        public Paciente paciente { get; set; }
     }
 }
--- DataAccess/EntityModelFundabien/entities/RegistroMedico.cs
+++ DataAccess/EntityModelFundabien/entities/RegistroMedico.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityModelFundabien.entities
 {
     public class RegistroMedico
     {
+        [Key]
         public double idRegistroMedico { get; set; }
         public double idPaciente { get; set; }
         public DateTime fechaAdmision { get; set; }
