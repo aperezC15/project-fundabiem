4a9fbc41428e47ba11f13a6d137c9b0d9e28f0fc
Daniel Estupe <upegonzalez897@gmail.com>
2019-10-25 13:21:50 -0600

    Se ha agregado la restricción Unique en el campo dpi de Persona

--- BrokerServices/common/DbContext.cs
+++ BrokerServices/common/DbContext.cs
@@ -62,6 +62,7 @@ namespace BrokerServices.common
             modelBuilder.ObjetivoDeIntervencionSeed();
 
             modelBuilder.PacienteConfig();
+            modelBuilder.PersonaConfig();
         }
     }
 }
