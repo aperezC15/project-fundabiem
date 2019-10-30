7993e9f29d4d427b4e7504811dfaa742a3268347
= <guillermo.pisqui@gmail.com>
2019-10-30 17:10:15 -0600

    controlador para cita

---
+++ BrokerServices/Migrations/20191030230922_citas.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BrokerServices.Migrations
+{
+    public partial class citas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Citas",
+                columns: table => new
+                {
+                    IdCita = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    dPaciente = table.Column<long>(nullable: false),
+                    IdTerapia = table.Column<long>(nullable: false),
+                    idEstado = table.Column<long>(nullable: false),
+                    edad = table.Column<int>(nullable: false),
+                    NoOrden = table.Column<string>(nullable: true),
+                    fechaCita = table.Column<DateTime>(nullable: false),
+                    fechaAsignacion = table.Column<DateTime>(nullable: false),
+                    AsignadoPor = table.Column<string>(nullable: true),
+                    pacienteidPaciente = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citas", x => x.IdCita);
+                    table.ForeignKey(
+                        name: "FK_Citas_Terapias_IdTerapia",
+                        column: x => x.IdTerapia,
+                        principalTable: "Terapias",
+                        principalColumn: "IdTerapia",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_EstadoCitas_idEstado",
+                        column: x => x.idEstado,
+                        principalTable: "EstadoCitas",
+                        principalColumn: "idEstado",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Citas_Pacientes_pacienteidPaciente",
+                        column: x => x.pacienteidPaciente,
+                        principalTable: "Pacientes",
+                        principalColumn: "idPaciente",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_IdTerapia",
+                table: "Citas",
+                column: "IdTerapia");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_idEstado",
+                table: "Citas",
+                column: "idEstado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Citas_pacienteidPaciente",
+                table: "Citas",
+                column: "pacienteidPaciente");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Citas");
+        }
+    }
+}
---
+++ DataAccess/EntityModelFundabien/entities/Citas.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityModelFundabien.entities
+{
+    public class Citas
+    {
+        [Key]
+        public Int64 IdCita { get; set; }
+        public Int64 dPaciente { get; set; }
+        public Int64 IdTerapia { get; set; }
+        public Int64 idEstado { get; set; }
+        public int edad { get; set; }
+        public string NoOrden { get; set; }
+        public DateTime fechaCita { get; set; }
+        public DateTime fechaAsignacion { get; set; }
+        public string AsignadoPor { get; set; }
+        //relaciones
+        public Paciente paciente { get; set; }
+        public Terapias Terapia { get; set; }
+        public EstadoCitas estadoCita { get; set; }
+    }
+}
