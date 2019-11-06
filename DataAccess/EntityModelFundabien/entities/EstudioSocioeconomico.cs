2de16f6b01b60db74b647ef600735de7309dfc1a
Daniel Estupe <upegonzalez897@gmail.com>
2019-11-05 21:10:48 -0600

    Se han agregado las entidades para estudio socioeconomico

---
+++ DataAccess/EntityModelFundabien/entities/EstudioSocioeconomico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityModelFundabien.entities
+{
+    public class EstudioSocioeconomico
+    {
+        public long Id { get; set; }
+        public long idPaciente { get; set; }
+        public DateTime fechaEntrevista { get; set; }
+        public string diagnosticoSocial { get; set; }
+        public string opinionTS { get; set; }
+        public string observaciones { get; set; }
+    }
+}
