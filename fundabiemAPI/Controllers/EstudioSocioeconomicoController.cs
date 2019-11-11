e2ad753b9bc5b40d01e70c40b1c32e2eec685266
Daniel Estupe <upegonzalez897@gmail.com>
2019-11-11 13:37:23 -0600

    Se ha agregado un endpoint para listar los estudios socioeconómicos

--- fundabiemAPI/Controllers/EstudioSocioeconomicoController.cs
+++ fundabiemAPI/Controllers/EstudioSocioeconomicoController.cs
@@ -13,6 +13,7 @@ using fundabiemAPI.clssResponses;
 
 namespace fundabiemAPI.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class EstudioSocioeconomicoController : coreControllerFundabiem<EstudioSocioeconomicoController>
@@ -36,7 +37,40 @@ namespace fundabiemAPI.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<clsResponse<SeccionEstudioSocioeconomicoDTO>>> get()
+        public async Task<ActionResult<clsResponse<EstudioSocioeconomicoDTO>>> getAllEstudiosSocioeconomicos(int pagina, int rowsPerPAge)
+        {
+            try
+            {
+                logger.LogInformation("Obteniendo una lista de EstudioSocioeconomico");
+                var respuesta = await fundabiem.getAllEstudioSocioeconomico(pagina, rowsPerPAge);
+                return Ok(respuesta);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Ha ocurrido un error al obtener la lista de estudio socioeconómico", ex.Message);
+                return BadRequest("ocurrió un error al obtener la lista de estudio socioeconómico");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EstudioSocioeconomicoDTO>> getEstudioSocioeconomico(long id)
+        {
+            try
+            {
+                logger.LogInformation("Obteniendo el EstudioSocioeconomico con ID {0}.", id);
+                var respuesta = await fundabiem.getEstudioSocioeconomicoById(id);
+                return Ok(respuesta);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Ha ocurrido un error al obtener el estudio socioeconómico con Id {id}. ", ex.Message);
+                return BadRequest($"ocurrió un error al obtener el estudio socioeconómico con Id {id}.");
+            }
+        }
+
+
+        [HttpGet("secciones")]
+        public async Task<ActionResult<clsResponse<SeccionEstudioSocioeconomicoDTO>>> getSecciones()
         {
             try
             {
@@ -58,7 +92,7 @@ namespace fundabiemAPI.Controllers
             {
                 logger.LogInformation("Creating: EstudioSocioeconomico.");
                 var respuesta = await fundabiem.newEstudioSocioeconomico(modelo);
-                return Ok(respuesta);
+                return CreatedAtAction("getEstudioSocioeconomico", new { id = respuesta.Id }, respuesta );
             }
             catch (Exception ex)
             {
